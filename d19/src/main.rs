3e5f404341523db1915e0f2b23b6b547f97c94c9
imDema <luca.de.martini@protonmail.com>
2020-12-19 15:03:47 +0100

    Day 19 part 1

---
+++ d19/src/main.rs
@@ -0,0 +1,104 @@
+use std::collections::HashMap;
+use std::io::{stdin, Read};
+
+#[derive(Debug)]
+enum Rule {
+    Simple(String),
+    Ref(Vec<Vec<usize>>),
+}
+
+fn parse_rule(s: &str) -> (usize, Rule) {
+    let (n, rest) = s.split_at(s.find(": ").expect("Invalid format"));
+    let id: usize = n.parse().expect("Invalid id format");
+    let rule = if rest.contains("\"") {
+        let content = rest.split("\"").nth(1).expect("Invalid literal");
+        Rule::Simple(content.to_owned())
+    } else {
+        let mut or = Vec::new();
+        let mut and = Vec::new();
+        for tok in rest[2..].split_whitespace() {
+            match tok {
+                "|" => {
+                    or.push(and); and = Vec::new()
+                },
+                _ => {
+                    let num: usize = tok.parse().expect("Cannot parse rule num");
+                    and.push(num);
+                }
+            }
+        }
+        or.push(and);
+        Rule::Ref(or)
+    };
+    (id, rule)
+}
+
+type RuleMap = HashMap<usize, Rule>;
+fn parse_rules(s: &str) -> Result<RuleMap, &'static str> {
+    let mut map = HashMap::new();
+    for l in s.split_terminator("\n") {
+        let (id, rule) = parse_rule(l);
+        map.insert(id, rule);
+    }
+    Ok(map)
+}
+
+fn concat_partial(head: &str, tail: Vec<String>) -> Vec<String> {
+    tail.into_iter()
+        .map(|t| {
+            let mut h = head.to_owned();
+            h.push_str(&t);
+            h
+        }).collect()
+}
+
+fn eval_seq(rules: &RuleMap, and: &[usize], input: &str) -> Vec<String> {
+    match and {
+        [x] => eval_rule(rules, *x, input),
+        v => {
+            let h = eval_rule(rules, v[0], input);
+            h.into_iter().flat_map(|head| {
+                let next_match = eval_seq(rules, &and[1..], &input[head.len()..]);
+                concat_partial(&head, next_match)
+            })
+            .collect()
+        }
+    }
+}
+
+fn eval_rule(rules: &RuleMap, id: usize, input: &str) -> Vec<String> {
+    let r = rules.get(&id).expect("Missing rule");
+    match r {
+        Rule::Simple(pat) => {
+            if input.starts_with(pat) {
+                return vec![pat.clone()];
+            } else {
+                return vec![]
+            }
+        },
+        Rule::Ref(v) => {
+            v.into_iter()
+                .flat_map(|or| eval_seq(rules, &or, input))
+                .collect()
+        }
+    }
+}
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+    let (rules, strings) = buf.split_at(buf.find("\n\n").unwrap());
+    let strings = &strings[2..];
+
+    let rules = parse_rules(rules).expect("Could not generate map");
+
+    let mut sum = 0;
+    for l in strings.split_terminator("\n") {
+        let vs = eval_rule(&rules, 0, l);
+        eprintln!("{} : {:?}", &l, &vs);
+        if vs.into_iter().any(|v| v == l) {
+            sum += 1;
+        }
+    }
+    println!("{}", sum);
+}
